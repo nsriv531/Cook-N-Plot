f8ea7274ca909e608f3700816c72c709470905d8
tai-shis <tshis070@mtroyal.ca>
2025-02-15 15:40:37 -0700

    Linking through pages added (shell)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Search from "./pages/search";
+import Home from "./pages/Home";
 
 function App() {
-  useEffect(() => {
-    const aos_init = () => {
-      AOS.init({
-        once: true,
-        duration: 1000,
-        easing: 'ease-out-cubic',
-      });
-    }
-
-    window.addEventListener('load', () => {
-      aos_init();
-    });
-  }, []);
-
   return (
     <Router>
       <Routes>
-        <Route path="/Search" element={<Search />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div >
       <header className="font-sans">
-          Welcome to Cook 'N Plot'
+          Welcome to Cook 'N Plot
+          <br></br>
+          <Link to="/Search">Search</Link>
       </header>
     </div>
   );
