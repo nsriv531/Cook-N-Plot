165bc3bf1e358ec72a886af242ac3d733d3d16e3
vgrin803 <vgrin803@mtroyal.ca>
2025-02-15 16:08:03 -0700

    fixed the button to link to /search

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 function StartButton() {
   return (
     <div className="flex flex-col items-center" >
+        <Link to="/Search">
         <button class="bg-green-500 hover:bg-green-700 text-black font-bold py-4 px-8 border border-green-700 rounded">
-        <Link to="/Search">Start</Link>
+        Start
         </button>
+        </Link>
     </div>
   );
 }
