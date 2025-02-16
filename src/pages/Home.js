80c7b7072d5947809165fe693125f9f662befff1
nsriv531 <nsriv531@mtroyal.ca>
2025-02-16 02:12:20 -0700

    ya

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
 import React from "react";
 import StartButton from "../components/StartButton";
 import GardenImage from "../components/GardenImage";
-import ChickenGif from "../components/ChickenGif"; // Import animated GIF
+import ChickenGif from "../components/ChickenGif"; 
+import PlantPic from "../components/PlantPic"; 
 
 const Home = () => {
   return (
-    <> 
-      <GardenImage />
+    <>
+      <div className="relative garden-wrapper">
+        <GardenImage />
+        <PlantPic /> {/* Plants positioned over garden holes */}
+      </div>
+
       <div className="relative h-[500px]">
         <ChickenGif /> {/* Moving chicken GIF */}
       </div>
+      
       <StartButton />
     </>
   );
