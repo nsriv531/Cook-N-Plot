aedf43bd7ac669b99fd01e4778e80fc2afb77298
tai-shis <tshis070@mtroyal.ca>
2025-02-16 10:58:35 -0700

    other flowers

---
+++ src/components/Purple2.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PurpleFlower2 from "../assets/purple2.png"; // Adjust the path if necessary
+
+const Purple2 = () => {
+    return (
+      <div class="absolute scale-200 z-9">
+        <img 
+          src={PurpleFlower2} 
+          alt="Purple Flower 2" 
+          
+        />
+      </div>
+    );
+  };
+  
+  export default Purple2;
