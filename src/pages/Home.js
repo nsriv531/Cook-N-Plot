2ef3187d6c3954cf5449bef6a9ee65c34c535144
tai-shis <tshis070@mtroyal.ca>
2025-02-16 10:35:01 -0700

    fixing header issues

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,17 +9,17 @@ import Title from "../components/Title";
 const Home = () => {
   return (
     <div className="relative min-h-screen w-full overflow-hidden">
-      <GardenImage />
+      <GardenImage /> 
 
-      <div className="absolute top-5 w-full text-center z-10">
-        <Title />
+      <div className="absolute top-5 w-full text-center z-5">
+      <Title />
       </div>
 
-      <div className="absolute top-20 left-5 z-10">
+      <div className="absolute top-20 left-5 z-5">
         <Blue5 />
       </div>
 
-      <div className="absolute inset-0 flex items-center justify-center z-10">
+      <div className="absolute inset-0 flex items-center justify-center z-6">
         <StartButton />
       </div>
     </div>
