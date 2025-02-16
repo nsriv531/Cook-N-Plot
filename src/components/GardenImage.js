78902f3c3fc2f3101cc225cfbbc901de3309201d
tai-shis <tshis070@mtroyal.ca>
2025-02-16 08:15:13 -0700

    tweaking some grass things

--- src/components/GardenImage.js
+++ src/components/GardenImage.js
@@ -3,13 +3,12 @@ import Grass from "../assets/grass.png"; // Adjust the path if necessary
 
 const GardenImage = () => {
   return (
-    <div className="relative z-negative">
-      <div class="absolute inset-0 z-negative bg-cover bg-center bg-fixed">
-        <img 
-          src={Grass} 
-          alt="Base" 
-        />
-      </div>
+    <div class="flex absolute bg-cover bg-center bg-fixed">
+      <img 
+        src={Grass} 
+        alt="Base" 
+        
+      />
     </div>
   );
 };
